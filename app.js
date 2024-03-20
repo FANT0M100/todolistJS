3702537349090be16f0923839820cc00fd581ca9
FANT0M100 <giorgie103188@gmail.com>
2024-03-20 18:12:19 +0400

    delete an mark li tag

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const todoList = document.querySelector(".todo-list");
 
 //Event Listener
 todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
 
 //Functions
 function addTodo(e) {
@@ -36,4 +37,20 @@ function addTodo(e) {
 
   //clear input value
   todoInput.value = "";
+}
+
+function deleteCheck(e) {
+  const item = e.target;
+
+  //delete todo
+  if (item.classList[0] === "trash-btn") {
+    const todo = item.parentElement;
+    todo.remove();
+  }
+
+  //check mark
+  if (item.classList[0] === "complete-btn") {
+    const todo = item.parentElement;
+    todo.classList.toggle("completed");
+  }
 }
